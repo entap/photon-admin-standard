0d57d4e34c53a421121f5c81478f59ce520c2a28
松岡 利昌 <matsuoka@entap.co.jp>
2018-07-08 21:56:49 +0900

    a

--- web/system/photon/config.php
+++ web/system/photon/config.php
@@ -1,14 +1,12 @@
 <?php
 
-ini_set('display_errors', 0);
-
 // データベースの設定
 config('db_hostname', 'localhost');
-config('db_username', 'photon-admin-standard');
-config('db_password', 'fyfwf6JrmDsJ');
-config('db_database', 'photon-admin-standard');
+config('db_username', '');
+config('db_password', '');
+config('db_database', '');
 
 // セッションの暗号化キー(32文字)
-config('secret_key', '012345678901234567890123456789012');
+config('secret_key', '');
 
 ?>
