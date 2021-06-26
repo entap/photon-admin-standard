d11a3101f5d4abe227df178c6245fef7ead110b4
松岡 利昌 <matsuoka@entap.co.jp>
2021-06-26 18:39:02 +0900

    Revert "openssl"
    This reverts commit e6e17e7d7f78b36d55deb1196bc3608745cbcc3c.

--- web/admin/view/common/footer.php
+++ web/admin/view/common/footer.php
@@ -3,10 +3,10 @@
 	<hr>
 	<p class="text-center">Copyright &copy; 2018 Entap,Inc. All rights reserved.</p>
 </footer>
-<script src="/assets/js/jquery.min.js"></script>
-<script src="/assets/js/bootstrap.min.js"></script>
-<script src="/assets/js/underscore-min.js"></script>
-<script src="/assets/js/bootstrap-remote-modal.js"></script>
-<script src="/assets/js/jquery-vif.js"></script>
+<script src="/admin/assets/js/jquery.min.js"></script>
+<script src="/admin/assets/js/bootstrap.min.js"></script>
+<script src="/admin/assets/js/underscore-min.js"></script>
+<script src="/admin/assets/js/bootstrap-remote-modal.js"></script>
+<script src="/admin/assets/js/jquery-vif.js"></script>
 </body>
 </html>
